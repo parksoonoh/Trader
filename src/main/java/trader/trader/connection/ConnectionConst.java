25aad289602c7b13ea69435596774f7a185dcc45
parksoonoh <62783397+parksoonoh@users.noreply.github.com>
2023-11-22 21:10:25 +0900

    type: project init

---
+++ src/main/java/trader/trader/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package trader.trader.connection;
+
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:h2:tcp://192.168.56.1/~/trader";
+    public static final String USERNAME = "trader";
+    public static final String PASSWORD = "trader";
+}
